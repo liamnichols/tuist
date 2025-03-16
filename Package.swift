f12ccfca00012c38188a734e2fc5c56755a68975
Mike Simons <msimons@etsy.com>
2025-03-16 12:54:29 -0400

    Comment out normal graph dependency

--- Package.swift
+++ Package.swift
@@ -521,7 +521,7 @@ let package = Package(
             url: "https://github.com/apple/swift-openapi-urlsession", .upToNextMajor(from: "1.0.2")
         ),
         .package(url: "https://github.com/tuist/Path", .upToNextMajor(from: "0.3.0")),
-        .package(url: "https://github.com/tuist/XcodeGraph.git", exact: "1.8.8"),
+//        .package(url: "https://github.com/tuist/XcodeGraph.git", exact: "1.8.8"),
         .package(url: "https://github.com/tuist/XcodeGraph.git", branch: "waltflaagan/StronglyTypedBuildSettings"),
         .package(url: "https://github.com/tuist/FileSystem.git", .upToNextMajor(from: "0.7.0")),
         .package(url: "https://github.com/tuist/Command.git", .upToNextMajor(from: "0.8.0")),
